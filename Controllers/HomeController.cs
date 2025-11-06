7146d720f2f65765b61ae228f5aaad76f51a358d
MigzReyes <johnmigzreyes0@gmail.com>
2025-11-07 00:39:31 +0800

    feat: Passengers finished

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -25,11 +25,22 @@ public class HomeController : Controller
 
     public IActionResult Itinerary()
     {
+        ViewBag.Title = "Itinerary";
+        ViewBag.Page = "Passengers";
         return View("Booking/Itinerary");
     }
 
     public IActionResult Passengers()
     {
+        ViewBag.Title = "Passengers";
+        ViewBag.Page = "Confirmation";
         return View("Booking/Passengers");
+    }
+
+    public IActionResult Confirmation()
+    {
+        ViewBag.Title = "Confirmation";
+        ViewBag.Page = "Payment";
+        return View("Booking/Confirmation");
     }
 }
